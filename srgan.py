d68bfc1873f39297fcb31ad3f6e7fc2d2e08dfc9
Vitor Loura <vitor.loura@intelie.com.br>
2021-06-17 20:24:04 -0300

    Atualizando GAN

--- srgan.py
+++ srgan.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import time
+from keras.layers.core import Flatten
 
 import numpy as np
 import tensorflow as tf
@@ -9,8 +10,9 @@ from contextlib import contextmanager
 
 from imageio import imread, imwrite
 from keras import Input
+from keras import backend as K
 from keras_preprocessing.image import img_to_array, load_img
-from keras.layers import BatchNormalization, Activation, LeakyReLU, Add, Dense
+from keras.layers import BatchNormalization, Activation, LeakyReLU, Add, Dense, PReLU
 from keras.layers.convolutional import Conv2D, UpSampling2D
 from tensorflow.keras.applications import VGG19
 from keras.models import Model, load_model
@@ -99,52 +101,82 @@ class DataLoader:
 data_loader = DataLoader()
 
 
+def preprocess_HR(x):
+    return np.divide(x.astype(np.float32), 127.5) - np.ones_like(
+        x, dtype=np.float32)
+
+
+def deprocess_HR(x):
+    x = (x + 1) * 127.5
+    return x.astype(np.uint8)
+
+
+def preprocess_LR(x):
+    return np.divide(x.astype(np.float32), 255.)
+
+
+def deprocess_LR(x):
+    x = np.clip(x * 255, 0, 255)
+    return x
+
+
+def normalize(input_data):
+    return (input_data.astype(np.float32) - 127.5) / 127.5
+
+
+def denormalize(input_data):
+    input_data = (input_data * 127.5) + 127.5
+    return input_data.astype(np.uint8)
+
+
 def build_vgg():
     # """
     # Builds a pre-trained VGG19 model that outputs image features extracted at the
     # third block of the model
     # """
-
-    base_model = VGG19(
-        weights="imagenet",
-        include_top=False,
-        input_shape=hr_shape,
-    )
-    base_model.trainable = False
-
-    inputs = Input(shape=hr_shape)
-
-    x = base_model(inputs, training=False)
-
-    outputs = Conv2D(256, (3, 3),
-                     activation='relu',
-                     padding='same',
-                     name='custom_block_conv')(x)
-
-    model = Model(inputs, outputs)
+    vgg19 = VGG19(include_top=False, weights='imagenet', input_shape=hr_shape)
+    vgg19.trainable = False
+    # Make trainable as False
+    for l in vgg19.layers:
+        l.trainable = False
+    model = Model(inputs=vgg19.input,
+                  outputs=vgg19.get_layer('block3_conv4').output)
+    model.trainable = False
 
     return model
+
+
+vgg_model = build_vgg()
+
+
+def vgg_loss(y_true, y_pred):
+    return K.mean(K.square(vgg_model(y_true) - vgg_model(y_pred)))
 
 
 def build_generator():
     def residual_block(layer_input, filters):
         """Residual block described in paper"""
-        block = Conv2D(filters, kernel_size=3, strides=1,
-                       padding='same')(layer_input)
-        block = Activation('relu')(block)
-        block = BatchNormalization(momentum=0.8)(block)
+        block = Conv2D(filters=filters,
+                       kernel_size=3,
+                       strides=1,
+                       padding="same")(layer_input)
+        block = BatchNormalization(momentum=0.5)(block)
+        block = PReLU(alpha_initializer='zeros',
+                      alpha_regularizer=None,
+                      alpha_constraint=None,
+                      shared_axes=[1, 2])(block)
         block = Conv2D(filters, kernel_size=3, strides=1,
                        padding='same')(block)
-        block = BatchNormalization(momentum=0.8)(block)
-        block = Add()([block, layer_input])
+        block = BatchNormalization(momentum=0.5)(block)
+        block = Add()([layer_input, block])
         return block
 
     def deconv2d(layer_input):
         """Layers used during upsampling"""
-        up_sampling = UpSampling2D(size=2)(layer_input)
         up_sampling = Conv2D(256, kernel_size=3, strides=1,
-                             padding='same')(up_sampling)
-        up_sampling = Activation('relu')(up_sampling)
+                             padding='same')(layer_input)
+        up_sampling = UpSampling2D(size=2)(up_sampling)
+        up_sampling = LeakyReLU(alpha=0.2)(up_sampling)
         return up_sampling
 
     # Low resolution image input
@@ -152,7 +184,10 @@ def build_generator():
 
     # Pre-residual block
     conv1 = Conv2D(64, kernel_size=9, strides=1, padding='same')(img_lr)
-    conv1 = Activation('relu')(conv1)
+    conv1 = PReLU(alpha_initializer='zeros',
+                  alpha_regularizer=None,
+                  alpha_constraint=None,
+                  shared_axes=[1, 2])(conv1)
 
     # Propogate through residual blocks
     residual_blocks = residual_block(conv1, generator_filters)
@@ -162,19 +197,21 @@ def build_generator():
     # Post-residual block
     conv2 = Conv2D(64, kernel_size=3, strides=1,
                    padding='same')(residual_blocks)
-    conv2 = BatchNormalization(momentum=0.8)(conv2)
-    conv2 = Add()([conv2, conv1])
+    conv2 = BatchNormalization(momentum=0.5)(conv2)
+    conv2 = Add()([conv1, conv2])
 
     # Upsampling
     up_sampling1 = deconv2d(conv2)
     up_sampling2 = deconv2d(up_sampling1)
 
     # Generate high resolution output
-    gen_hr = Conv2D(channels,
-                    kernel_size=9,
-                    strides=1,
-                    padding='same',
-                    activation='tanh')(up_sampling2)
+    gen_hr = Conv2D(
+        channels,
+        kernel_size=9,
+        strides=1,
+        padding='same',
+    )(up_sampling2)
+    gen_hr = Activation('tanh')(gen_hr)
 
     return Model(img_lr, gen_hr)
 
@@ -184,69 +221,69 @@ def build_discriminator():
         """Discriminator layer"""
         dis = Conv2D(filters, kernel_size=3, strides=strides,
                      padding='same')(layer_input)
-        dis = LeakyReLU(alpha=0.2)(dis)
         if bn:
-            dis = BatchNormalization(momentum=0.8)(dis)
+            dis = BatchNormalization(momentum=0.5)(dis)
+        dis = LeakyReLU(alpha=0.2)(dis)
         return dis
 
     # Input img
-    dis0 = Input(shape=hr_shape)
+    dis_input = Input(shape=hr_shape)
 
-    dis1 = dis_block(dis0, discriminator_filters, bn=False)
-    dis2 = dis_block(dis1, discriminator_filters, strides=2)
-    dis3 = dis_block(dis2, discriminator_filters * 2)
-    dis4 = dis_block(dis3, discriminator_filters * 2, strides=2)
-    dis5 = dis_block(dis4, discriminator_filters * 4)
-    dis6 = dis_block(dis5, discriminator_filters * 4, strides=2)
-    dis7 = dis_block(dis6, discriminator_filters * 8)
-    dis8 = dis_block(dis7, discriminator_filters * 8, strides=2)
+    dis = dis_block(dis_input, discriminator_filters, bn=False)
+    dis = dis_block(dis, discriminator_filters, strides=2)
+    dis = dis_block(dis, discriminator_filters * 2)
+    dis = dis_block(dis, discriminator_filters * 2, strides=2)
+    dis = dis_block(dis, discriminator_filters * 4)
+    dis = dis_block(dis, discriminator_filters * 4, strides=2)
+    dis = dis_block(dis, discriminator_filters * 8)
+    dis = dis_block(dis, discriminator_filters * 8, strides=2)
 
-    dis9 = Dense(discriminator_filters * 16)(dis8)
-    dis10 = LeakyReLU(alpha=0.2)(dis9)
-    validity = Dense(1, activation='sigmoid')(dis10)
+    dis = Flatten()(dis)
+    dis = Dense(discriminator_filters * 16)(dis)
+    dis = LeakyReLU(alpha=0.2)(dis)
 
-    return Model(dis0, validity)
+    validity = Dense(1)(dis)
+    validity = Activation('sigmoid')(validity)
+
+    return Model(dis_input, validity)
 
 
-def sample_images(epoch):
+def sample_images(epoch=None, is_testing=False):
     os.makedirs('imgs/%s' % dataset_name, exist_ok=True)
 
     hr_imgs, lr_imgs = data_loader.load_data(batch_size=2, is_testing=True)
     hr_fakes = generator.predict(lr_imgs)
 
-    # hr_imgs = hr_imgs / 127.5 - 1.0
-    # lr_imgs = lr_imgs / 127.5 - 1.0
-    hr_fakes = (hr_fakes + 1.0) * 127.5
+    hr_fakes = denormalize(hr_fakes)
 
-    for hr_img, hr_gen in zip(hr_imgs, hr_fakes):
-        imwrite('imgs/{}/{}_{}.jpg'.format(dataset_name, epoch, 'original'),
-                hr_img.astype(np.uint8))
-        imwrite('imgs/{}/{}_{}.jpg'.format(dataset_name, epoch, 'generated'),
+    if not is_testing:
+        for hr_gen in hr_fakes:
+            imwrite(
+                'imgs/{}/{}_{}.jpg'.format(dataset_name, epoch, 'generated'),
                 hr_gen.astype(np.uint8))
 
+    else:
+        for hr_img, lr_img in zip(hr_imgs, lr_imgs):
+            imwrite('imgs/{}/{}.jpg'.format(dataset_name, 'hr'),
+                    hr_img.astype(np.uint8))
+            imwrite('imgs/{}/{}.jpg'.format(dataset_name, 'lr'),
+                    lr_img.astype(np.uint8))
 
-# We use a pre-trained VGG19 model to extract image features from the high resolution
-# and the generated high resolution images and minimize the mse between them
-vgg = build_vgg()
-vgg.trainable = False
-vgg.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
-
-# Build and compile the discriminator
-discriminator = build_discriminator()
-discriminator.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
 
 # Build the generator
 generator = build_generator()
+generator.compile(loss=vgg_loss, optimizer=optimizer)
 
-# High res. and low res. images
-img_hr = Input(shape=hr_shape)
+discriminator = build_discriminator()
+discriminator.compile(loss='binary_crossentropy',
+                      optimizer=optimizer,
+                      metrics=['accuracy'])
+
+# low res. images
 img_lr = Input(shape=lr_shape)
 
 # Generate high res. version from low res.
 fake_hr = generator(img_lr)
-
-# Extract image features of the generated img
-fake_features = vgg(fake_hr)
 
 # For the adversarial model we will only train the generator
 discriminator.trainable = False
@@ -254,14 +291,17 @@ discriminator.trainable = False
 # Discriminator determines validity of generated high res. images
 validity = discriminator(fake_hr)
 
-adversarial = Model([img_lr, img_hr], [validity, fake_features])
-adversarial.compile(loss=['binary_crossentropy', 'mse'],
-                    loss_weights=[1e-3, 1],
+adversarial = Model([img_lr], [fake_hr, validity])
+adversarial.compile(loss=[vgg_loss, 'binary_crossentropy', 'mse'],
+                    loss_weights=[1.0, 1e-3, 1.0],
                     optimizer=optimizer)
+# Remover mse em testes futuros
 
-epochs = 300
+epochs = 12750
 batch_size = 1
 sample_interval = 50
+
+sample_images(is_testing=True)
 
 with time_context('treino total'):
     with tf.device('/gpu:0') as GPU:
@@ -274,20 +314,21 @@ with time_context('treino total'):
 
                 # Sample images and their conditioning counterparts
                 imgs_hr, imgs_lr = data_loader.load_data(batch_size)
-                imgs_hr = imgs_hr / 127.5 - 1.0
-                imgs_lr = imgs_lr / 127.5 - 1.0
+                imgs_hr = normalize(imgs_hr)
+                imgs_lr = normalize(imgs_lr)
 
                 # From low res. image generate high res. version
                 fake_hr = generator.predict(imgs_lr)
 
-                valid = np.ones((batch_size, ) + disc_patch)
-                fake = np.zeros((batch_size, ) + disc_patch)
+                real_y = np.ones(
+                    batch_size) - np.random.random_sample(batch_size) * 0.2
+                fake_y = np.random.random_sample(batch_size) * 0.2
 
                 discriminator.trainable = True
 
                 # Train the discriminators (original images = real / generated = Fake)
-                d_loss_real = discriminator.train_on_batch(imgs_hr, valid)
-                d_loss_fake = discriminator.train_on_batch(fake_hr, fake)
+                d_loss_real = discriminator.train_on_batch(imgs_hr, real_y)
+                d_loss_fake = discriminator.train_on_batch(fake_hr, fake_y)
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
                 # ------------------
@@ -296,20 +337,20 @@ with time_context('treino total'):
 
                 # Sample images and their conditioning counterparts
                 imgs_hr, imgs_lr = data_loader.load_data(batch_size)
-                imgs_hr = imgs_hr / 127.5 - 1.0
-                imgs_lr = imgs_lr / 127.5 - 1.0
+                imgs_hr = normalize(imgs_hr)
+                imgs_lr = normalize(imgs_lr)
 
                 discriminator.trainable = False
 
                 # The generators want the discriminators to label the generated images as real
-                valid = np.ones((batch_size, ) + disc_patch)
+                real_y = np.ones(
+                    batch_size) - np.random.random_sample(batch_size) * 0.2
 
-                # Extract ground truth image features using pre-trained VGG19 model
-                image_features = vgg.predict(imgs_hr)
+                # # Extract ground truth image features using pre-trained VGG19 model
+                # image_features = vgg.predict(imgs_hr)
 
                 # Train the generators
-                g_loss = adversarial.train_on_batch([imgs_lr, imgs_hr],
-                                                    [valid, image_features])
+                g_loss = adversarial.train_on_batch(imgs_lr, [imgs_hr, real_y])
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
